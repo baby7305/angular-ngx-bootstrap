ae57b814ac78775baf11caf27d068136d23e74b5
baby7305 <songhaibei7305@163.com>
2018-12-17 09:52:48 +0800

    route配置official模块替换掉默认主页

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+
+const appRoutes: Routes = [
+  { path: '', component: OfficialComponent },
+  { path: 'official', component: OfficialComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +23,11 @@ import { OfficialComponent } from './official/official.component';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
